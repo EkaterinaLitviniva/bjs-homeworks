f0d7ffe0f425abc48de706f6e8fd46969d21983f
Ekaterina Litvinova <e.litvinova@icloud.com>
2020-05-10 01:32:05 +0300

    added corrections task.js 1.2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,43 +1,37 @@
+"use strict";
+
 function getResult(a,b,c){
     let d = Math.pow(b, 2) - 4 * a * c;
     let x1;
     let x2;
   if (d > 0){
-    x1 = (-b - Math.sqrt(b)) / 2 * a;
-    x2 = (-b + Math.sqrt(b)) / 2 * a;
+    x1 = (-b - Math.sqrt(d)) / 2 * a;
+    x2 = (-b + Math.sqrt(d)) / 2 * a;
+    return [x1, x2]
   } else if (d == 0){
     x1 = -b / 2 * a;
-    x2 = '';
+    return [x1]
   } else {
-    x1 = '';
-    x2 = '';
+    return []
   }
-return [x1, x2]
 }
 
 function getAverageMark(marks){
-    count = marks.length;
-    let averageMark 
-    if (count == 0){
-      averageMark = 0;
-    } else if (count > 5){
+    let summ = 0;
+    if (marks.length == 0){
+      return 0;
+    } else if (marks.length > 5){
         console.log('Не более 5 оценок!');
-        averageMark = marks.splice(0,4);
-    } else if (count <= 5){
-        let summ = 0;
-        for (let i = 0; i <= count; i++){
+        marks = marks.slice(0,4);
+    }
+        for (let i = 0; i <= marks.length-1; i++){
         summ += marks[i];
         } 
-        averageMark = summ / count;
-    }
-    return averageMark;
+    return summ / marks.length;
 }
 
 function askDrink(name,dateOfBirthday){
-    let data = new Date();
-    let yearOfBirtday = dateOfBirthday.getFullYear();
-    let yearToday = data.getFullYear();
-    let age = yearToday - yearOfBirtday;
+    let age = new Date().getFullYear() - dateOfBirthday.getFullYear();
     let result = (age >= 18) ? `Не желаете ли олд-фэшн, ${name}?` : `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
     return result;
 }
