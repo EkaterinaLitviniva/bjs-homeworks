b5753d353dff591afd80c25d14cedd3889693207
Ekaterina Litvinova <e.litvinova@icloud.com>
2020-06-06 15:19:32 +0300

    added task.js

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,29 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    if (animal == undefined){
+    return null;
+    } else {
+    let animalSound = animal.sound;
+    return animalSound;
+    }
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let summ = 0;
+    if (marks.length == 0){
+    return 0;
+    }else {
+    for (let i = 0; i <= marks.length-1; i++){
+        summ += Number(marks[i]);
+        } 
+    let roundedAverage = Math.round(summ / marks.length);
+    return roundedAverage;
+    }
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = Math.round(new Date().getTime()/1000.0);
+    let b = Number(Date.parse(birthday)) / 1000.0;
+    let age = Math.round((now - b) / 31556926); 
+    let verdict = (age >= 18) ? "Да" : "Нет";
+    return verdict;
 }
