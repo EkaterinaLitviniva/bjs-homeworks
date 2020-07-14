704dcd970ac08884f695d25d03e83197842dd381
Ekaterina Litvinova <e.litvinova@icloud.com>
2020-07-14 14:30:23 +0300

    Added task.js

--- 3.3-async/task.js
+++ 3.3-async/task.js
@@ -1,18 +1,74 @@
+'use strict';
+
 class AlarmClock {
+
     constructor(){
-        this.alarmCollections = [];
+        this.alarmCollection = [];
         this.timerId = [];
     }
-    addClock(time,callBack,alarmId){
-        const exist = this.alarmCollections.id.find(alarm => alarm.id == alarmId);
-        if (alarmId == undefined){
-            throw new Error('error text');
-        } else if (exist){
-            return console.error(e);
-        };
-        this.alarmCollections.push({id: alarmId, time: time, callBack: callBack})
+    aaddClock(time, callback, id){
+        if (id === undefined) {
+      console.error('Не задан идентификатор таймера');
+      return;
+        }
+        if (this.alarmCollection.find(item => item.id === id)) {
+      console.error('Таймер с заданным идентификатором уже существует');
+      return;
+        }
+        this.alarmCollection.push({ id, time, callback });
     };
-    removeClock(alarmId){
-        
-    }
+
+    removeClock(id){
+        const alarm = this.id.filter(al => al.id === id)
+        if(alarm){
+            delete this.id[alarm];
+            return 'Звонок удален';
+        }
+         return 'Звонок не найден';
+    };
+
+    getCurrentFormattedTime(){
+        let now = new Date();
+        return `${now.getHours()}:${now.getMinutes()}`;
+    };
+
+    start(){
+        let checkClock = (alarm) => {
+            if (alarm.time == this.getCurrentFormattedTime()){ alarm.callback();}
+        }
+        this.timerId = setInterval(() => this.alarmCollection.forEach(element => checkClock(element)),1000);
+    };
+
+    stop(){
+        if (this.timerID !== null) {
+            clearInterval(this.timerID);
+            this.timerId = null;
+        }
+    };
+
+    printAlarms() {
+        this.alarmCollection.forEach(item => console.log(`Будильник №${item.id} заведён на ${item.time}`));
+      };
+
+    clearAlarms() {
+        this.stop;
+        this.alarmCollection = [];
+    };
 };
+
+function testCase(){
+    const phoneAlarm = new AlarmClock;
+    phoneAlarm.addClock('09:00',() => console.log('Пора вставать'),1);
+    phoneAlarm.addClock('09:05',() => {console.log('Давай, вставай уже!'); phoneAlarm.removeClock(2)},2);
+    phoneAlarm.addClock('09:10',() => console.log('Иди умываться'));
+    phoneAlarm.addClock('09:12',() => {
+        console.log('Вставай, а то проспишь!');
+    phoneAlarm.clearAlarms();
+    phoneAlarm.printAlarms();
+      },3);
+    phoneAlarm.addClock('09:13',()=>console.log('Вставай!'),1);
+    phoneAlarm.printAlarms();
+    phoneAlarm.start();
+};
+
+testCase();
